99a950b014807830d07c73a368f74e4c6144b7a3
1-StarKing <88681971+1-StarKing@users.noreply.github.com>
2022-12-11 02:29:04 -0600

    Created basic Models for user and admin

---
+++ HistoryBoothApp/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoryBoothApp.Models
+{
+    public class Admin
+    {
+        public string password { get; set; }
+
+        public List<UserRecording> userRecordings { get; set; }
+
+        public Admin()
+        {
+            password = "password";
+            userRecordings = new List<UserRecording>();
+        }
+    }
+}
---
+++ HistoryBoothApp/Models/UserRecording.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoryBoothApp.Models
+{
+    public class UserRecording
+    {
+        public string fullName { get; set; }
+
+        public string storyDecade { get; set; }
+
+        public bool wasStudent { get; set; }
+
+        //property for the recording mp3 itself
+        //public Taglib.File recording { get; set; }
+
+        //public Taglib.File ReadRecording()
+        //{
+        //    return recording;
+        //}
+
+        //public bool WriteRecording(Taglib.File value)
+        //{
+        //    return true;
+        //}
+    }
+}
